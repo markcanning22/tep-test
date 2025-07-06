5ddc073b3bb75167e4e056cd18eb2e84322ab713
Mark Canning <markcanning22@users.noreply.github.com>
2025-07-06 18:45:10 +0100

    wip - replaced hardcoded user with that from the request

--- apps/user/src/handlers/create-user-handler.ts
+++ apps/user/src/handlers/create-user-handler.ts
@@ -1,17 +1,16 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { createUser } from '../repositories/user-repository';
+import { NewUser } from '../types';
+
+export type CreateUserRequest = FastifyRequest<{
+  Body: NewUser;
+}>;
 
 export const createUserHandler = async (
-  _: FastifyRequest,
+  request: CreateUserRequest,
   reply: FastifyReply
 ) => {
-  await createUser({
-    firstName: 'Mark',
-    lastName: 'Canning',
-    password: 'changeme123',
-    email: 'mark@supplyant.com',
-    type: 'teacher',
-  });
+  const createdUser = await createUser(request.body);
 
-  reply.send({ message: 'Hello API' });
+  reply.send(createdUser);
 };
